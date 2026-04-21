dc11aaba129f550de386876a30ba75ddb9d25ee7
Suijester <akilesh.yes@gmail.com>
2026-04-21 15:14:53 -0700

    added config file, bug fixed foldshift_compile.sh

--- compile/foldshift_compile.sh
+++ compile/foldshift_compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT_FILE="distance_spectrum.cu"
+INPUT_FILE="../distance_spectrum.cu"
 OUTPUT_FILE="lib/libfoldshift.so"
 
 echo "Compiling $INPUT_FILE..."
@@ -8,7 +8,7 @@ echo "Compiling $INPUT_FILE..."
 # Create output directory if it doesn't exist
 mkdir -p ../lib
 
-nvcc -shared -Xcompiler -fPIC -O3 -gencode arch=compute_75,code=sm_75 distance_spectrum.cu -o lib/libfoldshift.so
+nvcc -shared -Xcompiler -fPIC -O3 -gencode arch=compute_75,code=sm_75 distance_spectrum.cu -o ../lib/libfoldshift.so
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful: $OUTPUT_FILE"
