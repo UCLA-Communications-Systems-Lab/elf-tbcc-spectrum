5db8747a62774d08171f9d401826cf1a41ef15c9
brucequz <brucequ@ucla.edu>
2026-05-28 11:52:41 -0700

    hotfix: output filename

--- distance_spectrum_computation/code_design.py
+++ distance_spectrum_computation/code_design.py
@@ -141,9 +141,7 @@ def main(config_path: str):
 
     # Output filename
     base_filename = f"k{config["bch_config"]["K"]}n{config["tbcc_config"]["N"]}v{config["tbcc_config"]["V"]}"
-    file_path = (
-        base_filename, "rename_needed"
-    ).with_suffix(".h5")
+    file_path = Path(f"{base_filename}").with_suffix(".h5")
     with h5py.File(file_path, "w") as f:
         pass
     print(f"Writing results to {file_path}")
